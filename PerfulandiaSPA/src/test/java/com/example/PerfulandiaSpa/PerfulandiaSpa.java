a5451bae9b4df181745a4cefcbae52bb4a0fc2da
Mirfak-pers <gabrielgamer498@gmail.com>
2025-05-07 11:29:09 -0400

    Add files via upload

---
+++ PerfulandiaSPA/src/test/java/com/example/PerfulandiaSpa/PerfulandiaSpa.java
@@ -0,0 +1,13 @@
+package com.example.PerfulandiaSpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaSpa {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
