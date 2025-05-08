f9fd50225cfeb1f2e9d97c75536914005b540cc6
pandequeso2 <ben.arayag@duocuc.cl>
2025-05-08 12:19:24 -0400

    Sucursal

---
+++ PerfulandiaSPA/src/main/java/com/example/PerfulandiaSpa/model/Sucursal.java
@@ -0,0 +1,31 @@
+package com.example.perfulandia.Model;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Table(name="Sucursal")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Sucursal {
+    @Id
+    private int id;
+    @Column(nullable = false)
+    private String direccion;
+    @Column(nullable = false)
+    private int horaApertura;
+    @Column(nullable = false)
+    private int horaCierre;
+}
