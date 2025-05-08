3d3fb6a9b97b13691c84f3e6008f109c93b2dafc
pandequeso2 <ben.arayag@duocuc.cl>
2025-05-08 11:16:59 -0400

    Reportes COntroller
    COntroller

---
+++ PerfulandiaSPA/src/main/java/com/example/PerfulandiaSpa/controller/ReporteInventarioController.java
@@ -0,0 +1,23 @@
+package com.example.bibliotecaDuocBDD.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bibliotecaDuocBDD.model.ReporteInventario;
+import com.example.bibliotecaDuocBDD.services.ReporteInventarioService;
+
+
+
+@RestController
+@RequestMapping("/ReporteInventario")
+public class ReporteInventarioController {
+    @Autowired
+    private ReporteInventarioService reporteInventarioService;
+        @PostMapping
+    public ReporteInventario createReporteI(@RequestBody ReporteInventario inventario) {
+        return reporteInventarioService.guardar(inventario);
+    }
+}
---
+++ PerfulandiaSPA/src/main/java/com/example/PerfulandiaSpa/controller/ReporteRendimientoController.java
@@ -0,0 +1,21 @@
+package com.example.bibliotecaDuocBDD.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bibliotecaDuocBDD.model.ReporteRendimiento;
+import com.example.bibliotecaDuocBDD.services.ReporteRendimientoService;
+
+@RestController
+@RequestMapping("/ReporteRendimiento")
+public class ReporteRendimientoController {
+    @Autowired
+    private ReporteRendimientoService reporteRendimientoService;
+    @PostMapping
+    public ReporteRendimiento createReporteR(@RequestBody ReporteRendimiento rendimiento) {
+        return reporteRendimientoService.guardar(rendimiento);
+    }
+}
