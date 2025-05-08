aad8509fbb96c45a17d606ba984a9f7b7fbe4b48
pandequeso2 <ben.arayag@duocuc.cl>
2025-05-08 11:25:43 -0400

    Producto
    Producto en general

---
+++ PerfulandiaSPA/src/main/java/com/example/PerfulandiaSpa/model/Producto.java
@@ -0,0 +1,33 @@
+package com.example.perfulandia.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "producto")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Producto {
+    @Id
+    public int id_producto;
+    @Column(nullable= false,length = 50)
+    public String nombre;
+    @Column(nullable= false,length=50)
+    public int precio;
+    @Column(nullable=false,length=50)
+    public int stock_total;
+    @Column(nullable=false,length=50)
+    public String categoria;
+    @Column(nullable=false,length=50)
+    public String estado;
+
+
+}
