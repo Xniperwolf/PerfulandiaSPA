8e53a1450d443a7f11d290b6b9bbab56ece8009e
pandequeso2 <ben.arayag@duocuc.cl>
2025-05-09 23:08:20 -0400

    PedidoService
    PedidoServcice

---
+++ PerfulandiaSPA/src/main/java/com/example/PerfulandiaSpa/services/PedidoService.java
@@ -0,0 +1,30 @@
+package com.example.perfulandia.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.perfulandia.Model.Pedido;
+import com.example.perfulandia.Repository.PedidoRepositoryJPA;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class PedidoService {
+    @Autowired
+    private PedidoRepositoryJPA pedidoRepo;
+    public List<Pedido> getPedidos(){
+        return pedidoRepo.findAll();
+    }
+    public Pedido getPedidoById(int id){
+        return pedidoRepo.findById(id);
+    }
+    public Pedido savePedido(Pedido ped){
+        return pedidoRepo.save(ped);
+    }
+    public Pedido updatePedido(Pedido ped){
+        return pedidoRepo.save(ped);
+    }
+}
