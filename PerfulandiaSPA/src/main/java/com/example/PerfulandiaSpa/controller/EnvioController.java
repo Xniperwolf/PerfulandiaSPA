438e5e00eccca3bdae6e3a2009e3cd4a270af8b9
pandequeso2 <ben.arayag@duocuc.cl>
2025-05-14 12:27:26 -0400

    ENviosController

---
+++ PerfulandiaSPA/src/main/java/com/example/PerfulandiaSpa/controller/EnvioController.java
@@ -0,0 +1,69 @@
+package com.example.PerfulandiaSpa.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Model.Envios;
+import com.example.demo.Service.EnvioService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("/api/v1/envios")
+public class EnvioController {
+    @Autowired
+    private EnvioService envioService;
+
+    @GetMapping()
+    public ResponseEntity<List<Envios>> getEnvios() {
+        List<Envios> listaEnvios= envioService.getEnvios();
+        if (listaEnvios.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(listaEnvios);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Envios> SaveEnvio(@RequestBody Envios envi) {
+        Envios envios=envioService.updatEnvios(envi);
+        return ResponseEntity.status(HttpStatus.CREATED).body(envios);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Envios> getByID(@PathVariable int id) {
+        try {
+            Envios envi=envioService.getPedidoByID(id);
+            return ResponseEntity.ok(envi);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<Envios> updateEnvios(@PathVariable int id, @RequestBody Envios env) {
+        try {
+            Envios envi=envioService.getPedidoByID(id);
+            envi.setEstado(env.getEstado());
+            envi.setDestino(env.getDestino());
+            envi.setOrigen(env.getOrigen());
+            envi.setListProducto(env.getListProducto());
+            envi.setSucursal(env.getSucursal());
+            envioService.updatEnvios(envi);
+            return ResponseEntity.ok(envi);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    
+    
+}
